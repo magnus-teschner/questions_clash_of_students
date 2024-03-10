84c365c3737124f0ee0837fc98e657bf04374530
lucamarie <80831645+lucamarie@users.noreply.github.com>
2024-03-10 13:43:57 +0100

    add course overview page

---
+++ course_overview/course-overview.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.toggle-btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+      const lessons = btn.parentNode.nextElementSibling;
+      lessons.style.display = lessons.style.display === 'block' ? 'none' : 'block';
+      btn.textContent = btn.textContent === '▼' ? '▲' : '▼';
+    });
+  });
+  
