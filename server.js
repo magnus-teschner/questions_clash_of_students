c5e97009f245fb281ebc10a1c7d7e52920009be8
Magnus Teschner <magnus.2002.mt@gmail.com>
2024-04-09 14:13:32 +0200

    Reworked whole UI and cleaned up REPO

--- server.js
+++ server.js
@@ -1,55 +1,16 @@
-const http = require('http');
-const amqp = require('amqplib');
 const express = require('express');
+const path = require('path');
 const app = express();
-const fs = require('fs');
-const cors = require('cors');
-app.set('view engine', 'ejs');
+const port = 3000;
+
+// Serve static files from the "public" directory
 app.use(express.static('public'));
-app.use(express.json());
 
-app.use(cors({
-    origin: '*'
-}));
-
-
-async function connectRabbitMQ() {
-    const rabbitHost = process.env.RABBITMQ_HOST
-    const connection = await amqp.connect(`amqp://rabbit`);
-    const channel = await connection.createChannel();
-    await channel.assertQueue('questionsQueue');
-    console.log("connected_succesfully")
-    return channel;
-}
-
-//const channelPromise = connectRabbitMQ();
-
-app.post('/submit_question', async (req, res) => {
-    const questionData = req.body;
-    console.log('Received question:', questionData);
-
-    const channel = await channelPromise;
-    channel.sendToQueue('questionsQueue', Buffer.from(JSON.stringify(questionData)));
-
-    res.json({ status: 'Success', message: 'Question sent to RabbitMQ' });
+// Send "questions.html" when the "/questions" route is accessed
+app.get('/questions', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/views', 'questions.html'));
 });
 
-
-app.get('/question', function(req, res) {	
-	res.render('questions.ejs', {});
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
-
-app.get('/login', function(req, res) {	
-	res.render('login.ejs', {});
-});
-
-app.get('/course', function(req, res) {	
-	res.render('course.ejs', {});
-});
-
-
-app.listen(3000);
-console.log("App is running");
-
-
-
