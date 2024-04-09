6d2918ac9c1c6d1908271c0bd6cb75790e1caefc
Magnus Teschner <magnus.2002.mt@gmail.com>
2024-04-09 17:46:14 +0200

    Add javascript that changes selected button

--- public/js/question.js
+++ public/js/question.js
@@ -1,1 +1,23 @@
 
+const elements = [
+    document.querySelector('#multiple-choice'),
+    document.querySelector('#multiple-choice-image'),
+    document.querySelector('#image-description'),
+    document.querySelector('#text-description')
+  ];
+  
+
+  function removeClassFromAll(className) {
+    elements.forEach(element => {
+      element.classList.remove(className);
+    });
+  }
+  
+
+  elements.forEach(element => {
+    element.addEventListener('click', function() {
+      removeClassFromAll('selected-mode');
+      this.classList.add('selected-mode');
+    });
+  });
+  
