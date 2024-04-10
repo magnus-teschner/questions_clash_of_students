57b6edd2074189ec12d069e2e957a35e1e297fb6
Magnus Teschner <magnus.2002.mt@gmail.com>
2024-04-10 10:08:17 +0200

    Added code to connect to mongo DB container

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const {MongoClient} = require('mongodb');
 const app = express();
 const port = 3000;
 
@@ -10,6 +11,36 @@ app.use(express.static('public'));
 app.get('/questions', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/views', 'questions.html'));
 });
+
+async function listDatabases(client){
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+async function connect_mongo(){
+  const uri = "mongodb://root:password@localhost:27017/test?authSource=admin";
+
+
+  const client = new MongoClient(uri);
+
+  try {
+      // Connect to the MongoDB cluster
+      await client.connect();
+
+      // Make the appropriate DB calls
+      await  listDatabases(client);
+
+  } catch (e) {
+      console.error(e);
+  } finally {
+      await client.close();
+  }
+}
+
+connect_mongo().catch(console.error);
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
