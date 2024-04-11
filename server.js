8b6bffc8eaec33c1af0a90876e2d60f769b43a32
Magnus Teschner <magnus.2002.mt@gmail.com>
2024-04-11 14:56:46 +0200

    Add functionality to upload and retrieve images from mongoDB

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const path = require('path');
-const {MongoClient} = require('mongodb');
+const multer = require("multer");
+const { GridFSBucket } = require('mongodb');
+const { GridFsStorage } = require("multer-gridfs-storage")
+const { ObjectId } = require("mongodb");
+const MongoClient = require("mongodb").MongoClient
 const app = express();
 const port = 3000;
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+const url = 'mongodb://root:password@localhost:27017/images?authSource=admin';
+const mongoClient = new MongoClient(url)
+
+
 
 // Serve static files from the "public" directory
 app.use(express.static('public'));
@@ -12,34 +23,93 @@ app.get('/questions', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/views', 'questions.html'));
 });
 
-async function listDatabases(client){
-  databasesList = await client.db().admin().listDatabases();
-
-  console.log("Databases:");
-  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
-
-async function connect_mongo(){
-  const uri = "mongodb://root:password@localhost:27017/test?authSource=admin";
-
-
-  const client = new MongoClient(uri);
-
-  try {
-      // Connect to the MongoDB cluster
-      await client.connect();
-
-      // Make the appropriate DB calls
-      await  listDatabases(client);
-
-  } catch (e) {
-      console.error(e);
-  } finally {
-      await client.close();
+app.post('/upload', upload.single('image'), (req, res) => {
+  if (!req.file) {
+      return res.status(400).send('No file uploaded');
   }
-}
+  
+  const bucket = new GridFSBucket(mongoClient.db("images"), {
+      bucketName: 'images'
+  });
 
-connect_mongo().catch(console.error);
+  const uploadStream = bucket.openUploadStream(req.file.originalname, {
+      contentType: req.file.mimetype
+  });
+
+  // Since req.file.buffer is a Buffer, we can directly write it to the upload stream
+  uploadStream.end(req.file.buffer);
+
+  uploadStream.on('error', (error) => {
+      return res.status(500).send(error);
+  });
+
+  uploadStream.on('finish', (file) => {
+      return res.status(201).send({
+          message: 'File uploaded successfully',
+          id: uploadStream.id.toString()
+      });
+  });
+});
+
+app.get("/images", async (req, res) => {
+try {
+  await mongoClient.connect()
+
+  const database = mongoClient.db("images")
+  const images = database.collection("images.files")
+  const cursor = images.find({})
+  const count = await cursor.count()
+  if (count === 0) {
+    return res.status(404).send({
+      message: "Error: No Images found",
+    })
+  }
+
+  const allImages = []
+
+  await cursor.forEach(item => {
+    allImages.push(item)
+  })
+
+  res.send({ files: allImages })
+} catch (error) {
+  console.log(error)
+  res.status(500).send({
+    message: "Error Something went wrong",
+    error,
+  })
+}
+})
+
+app.get("/download", async (req, res) => {
+try {
+  await mongoClient.connect()
+
+  const database = mongoClient.db("images");
+
+  const imageBucket = new GridFSBucket(database, {
+    bucketName: "images",
+  })
+
+  let downloadStream = imageBucket.openDownloadStream(new ObjectId(req.query.id)
+  )
+
+  downloadStream.on("data", function (data) {
+    return res.status(200).write(data)
+  })
+
+
+  downloadStream.on("end", () => {
+    return res.end()
+  })
+} catch (error) {
+  console.log(error)
+  res.status(500).send({
+    message: "Error Something went wrong",
+    error,
+  })
+}
+})
 
 
 app.listen(port, () => {
