d93fca073e5988f1242bfa51ea69e4e92867425b
lucamarie <80831645+lucamarie@users.noreply.github.com>
2024-05-16 15:50:57 +0200

    add profile dropdown

--- backend/public/js/course.js
+++ backend/public/js/course.js
@@ -12,4 +12,34 @@ document.querySelectorAll('.play-btn').forEach(btn => {
     var url = 'https://kocbetue.itch.io/clash-of-students';
     window.location.href = url;
   })
-})
+});
+
+//Öffnen und schließen des Profils bei Klick auf Icon
+function toggleProfile() {
+  var dropdown = document.getElementById('profile-dropdown');
+  var profileIcon = document.querySelector('.icon-profile');
+  var isOpen = dropdown.style.display === 'block';
+
+  if (!isOpen) {
+    dropdown.style.display = 'block';
+    profileIcon.focus();
+    document.addEventListener('click', closeProfileClickOutside);
+  } else {
+    dropdown.style.display = 'none';
+    profileIcon.blur();
+    document.removeEventListener('click', closeProfileClickOutside);
+  }
+}
+
+//Schließen des Profils bei Klick außerhalb von Icon und Dropdown
+function closeProfileClickOutside(event) {
+  var dropdown = document.getElementById('profile-dropdown');
+  var profileIcon = document.querySelector('.icon-profile');
+  var isClickInsideDropdown = dropdown.contains(event.target);
+  var isClickOnIcon = profileIcon.contains(event.target);
+
+  if (!isClickInsideDropdown && !isClickOnIcon) {
+    dropdown.style.display = 'none';
+    document.removeEventListener('click', closeProfileClickOutside);
+  }
+}
