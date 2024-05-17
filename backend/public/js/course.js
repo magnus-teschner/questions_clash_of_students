e893c16c7b6ce29ce6493860f5f4987201fb7b61
Magnus Teschner <magnus.2002.mt@gmail.com>
2024-05-17 10:41:09 +0200

    Use classes instead of inline style to show and unshow dropdown

--- backend/public/js/course.js
+++ backend/public/js/course.js
@@ -1,28 +1,34 @@
-var profileIcon = document.querySelector('.icon-profile');
-var dropdown = document.getElementById('profile-dropdown');
+const profileIcon = document.querySelector('#icon-profile');
+
+const dropdown = document.getElementById('profile-dropdown');
+
+
 
 document.addEventListener('click', function (event) {
   var isClickOnIcon = profileIcon.contains(event.target)
-  var isOpen = dropdown.style.display === 'block';
+  var isOpen = dropdown.classList.contains('show');
 
   if (isClickOnIcon) {
     if (!isOpen) {
-      dropdown.style.display = 'block';
+      dropdown.classList.remove('hidden');
+      dropdown.classList.add('show');
       profileIcon.focus();
     } else {
-      dropdown.style.display = 'none';
+      dropdown.classList.remove('show');
+      dropdown.classList.add('hidden');
       profileIcon.blur();
     }
   } else {
     if (!dropdown.contains(event.target)) {
-      dropdown.style.display = 'none';
+      dropdown.classList.remove('show');
+      dropdown.classList.add('hidden');
     } else {
       profileIcon.focus();
     }
   }
 });
 
-document.querySelector('.icon-logout').addEventListener('click', function () {
+document.querySelector('#icon-logout').addEventListener('click', function () {
   window.location.href = 'log-out';
 });
 
