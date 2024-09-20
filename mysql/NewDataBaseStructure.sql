2bace6a34580144b807b6608c0d9782e02443fa5
Magnus Teschner <78046527+magnus-teschner@users.noreply.github.com>
2024-09-20 16:27:43 +0200

    Update NewDataBaseStructure.sql

--- mysql/NewDataBaseStructure.sql
+++ mysql/NewDataBaseStructure.sql
@@ -1,3 +1,36 @@
+CREATE TABLE IF NOT EXISTS `accounts` (
+  `user_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `firstname` VARCHAR(50) NOT NULL,
+  `lastname` VARCHAR(50) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `role` ENUM('professor', 'student'),
+  `isVerified` BOOLEAN DEFAULT FALSE,
+  `verificationToken` VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS `programs` (
+    `program_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `program_name` VARCHAR(255)
+);
+
+
+CREATE TABLE IF NOT EXISTS `courses` (
+    `course_id` INT AUTO_INCREMENT PRIMARY KEY,
+    `course_name` VARCHAR(255) NOT NULL,
+    `program_id` INT,
+    `creator` INT,
+    FOREIGN KEY (`program_id`) REFERENCES `programs`(`program_id`),
+    FOREIGN KEY (`creator`) REFERENCES `accounts`(`user_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `lections` (
+  `lection_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `lection_name` VARCHAR(255) NOT NULL,
+  `course_id` INT NOT NULL,
+  FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`) ON DELETE CASCADE
+);
+
 CREATE TABLE IF NOT EXISTS `questions` (
     `question_id` INT AUTO_INCREMENT PRIMARY KEY,
     `user_id` INT,
@@ -15,21 +48,6 @@ CREATE TABLE IF NOT EXISTS `questions` (
     FOREIGN KEY (`user_id`) REFERENCES `accounts` (`user_id`) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS `lections` (
-  `lection_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `lection_name` VARCHAR(255) NOT NULL,
-  `course_id` INT NOT NULL,
-  FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`) ON DELETE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS `courses` (
-    `course_id` INT AUTO_INCREMENT PRIMARY KEY,
-    `course_name` VARCHAR(255) NOT NULL,
-    `program_id` INT,
-    `creator` INT,
-    FOREIGN KEY (`program_id`) REFERENCES `programs`(`program_id`),
-    FOREIGN KEY (`creator`) REFERENCES `accounts`(`user_id`)
-);
 
 CREATE TABLE IF NOT EXISTS `course_members` (
     `user_id` INT,
@@ -40,21 +58,6 @@ CREATE TABLE IF NOT EXISTS `course_members` (
     FOREIGN KEY (`course_id`) REFERENCES `courses`(`course_id`)
 );
 
-CREATE TABLE IF NOT EXISTS `programs` (
-    `program_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `program_name` VARCHAR(255)
-);
-
-CREATE TABLE IF NOT EXISTS `accounts` (
-  `user_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `firstname` VARCHAR(50) NOT NULL,
-  `lastname` VARCHAR(50) NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
-  `role` ENUM('professor', 'student'),
-  `isVerified` BOOLEAN DEFAULT FALSE,
-  `verificationToken` VARCHAR(255)
-);
 
 CREATE TABLE IF NOT EXISTS `scores` (
   `user_id` INT NOT NULL,
@@ -66,9 +69,9 @@ CREATE TABLE IF NOT EXISTS `scores` (
 CREATE TABLE IF NOT EXISTS `lection_scores` (
   `lection_id` INT,
   `user_id` INT,
-  'lection_score' INT
+  `lection_score` INT,
   FOREIGN KEY (`user_id`) REFERENCES `accounts` (`user_id`) ON DELETE CASCADE,
-  FOREIGN KEY (`lection_id`) REFERENCES `lections` (`lection_id`) ON DELETE CASCADE,
+  FOREIGN KEY (`lection_id`) REFERENCES `lections` (`lection_id`) ON DELETE CASCADE
 );
 
 INSERT INTO `programs`(`program_name`) VALUES ('Digital Business');
