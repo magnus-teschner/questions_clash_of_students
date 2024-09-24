7884e2f9f01108b1df858cb69c79f6fa6f640d58
Magnus Teschner <magnus.2002.mt@gmail.com>
2024-09-24 18:20:36 +0200

    Remove mailjet as a dependency

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,6 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage });
 const mysql = require('mysql2');
 require('dotenv').config();
-const Mailjet = require('node-mailjet');
 const { v4: uuidv4 } = require('uuid');
 
 const passport = require("passport");
@@ -51,9 +50,8 @@ const axios = require('axios');
 const makePutRequest = async (url, data = {}, headers = { 'Content-Type': 'application/json' }) => {
   try {
     const response = await axios.put(url, data, { headers });
-    return { data: response.data, status: response.status }; // Return both data and status
+    return { data: response.data, status: response.status };
   } catch (error) {
-    // Return the error response instead of throwing it
     return {
       error: true,
       data: error.response ? error.response.data : error.message,
@@ -65,9 +63,8 @@ const makePutRequest = async (url, data = {}, headers = { 'Content-Type': 'appli
 const makeGetRequest = async (url, headers = { 'Content-Type': 'application/json' }) => {
   try {
     const response = await axios.get(url, { headers });
-    return { data: response.data, status: response.status }; // Return both data and status
+    return { data: response.data, status: response.status };
   } catch (error) {
-    // Return the error response instead of throwing it
     return {
       error: true,
       data: error.response ? error.response.data : error.message,
@@ -79,9 +76,8 @@ const makeGetRequest = async (url, headers = { 'Content-Type': 'application/json
 const makePostRequest = async (url, data = {}, headers = { 'Content-Type': 'application/json' }) => {
   try {
     const response = await axios.post(url, data, { headers });
-    return { data: response.data, status: response.status }; // Return both data and status
+    return { data: response.data, status: response.status };
   } catch (error) {
-    // Return the error response instead of throwing it
     return {
       error: true,
       data: error.response ? error.response.data : error.message,
@@ -89,14 +85,6 @@ const makePostRequest = async (url, data = {}, headers = { 'Content-Type': 'appl
     };
   }
 };
-
-
-
-
-const mailjet = Mailjet.apiConnect(
-  mailjet_public_key,
-  mailjet_private_key
-);
 
 
 const config_mysql = {
